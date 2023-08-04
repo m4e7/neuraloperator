55a7dc804bd81e203db1ff3f539516945c69eeda
Mogab Elleithy <76572666+m4e7@users.noreply.github.com>
2023-08-04 13:58:08 -0700

    Run 2+1 dim NS with V=1e-4 dataset

--- neuralop/__init__.py
+++ neuralop/__init__.py
@@ -3,4 +3,5 @@ __version__ = '0.2.1'
 from . import datasets, mpu, layers
 from .models import TFNO3d, TFNO2d, TFNO1d, TFNO, get_model
 from .training import Trainer, LpLoss, H1Loss
-from .utils import count_params
+from .utils import count_params, UnitGaussianNormalizer
+
